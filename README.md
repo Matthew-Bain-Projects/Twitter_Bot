e830b7a4b29e4f283af7d20506eee2d4ea290f06
Matthew-Bain-Projects <matthewbain@protonmail.com>
2021-09-17 21:37:02 -0400

    Tweeting a Link

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Twitter_Bot
 
-James, don't abuse this
+Purpose: Tweet Updates to Projects I Am working on
+
+How it Works:
+
+Using the Tweepy API, I tweet a premade message and a link to the github page where my project lives. 
--- twitter_bot.py
+++ twitter_bot.py
@@ -1,10 +1,11 @@
 import requests
 import tweepy
+import keys
 
-consumer_key =
-consumer_secret =
-access_token = 
-access_token_secret =
+consumer_key = keys.consumer_key
+consumer_secret = keys.consumer_secret
+access_token = keys.access_token
+access_token_secret = keys.access_token_secret
  
 
 # Twitter requires oAuth2 to access its API:
@@ -16,4 +17,4 @@ def tweet_something(tweet):
     status = api.update_status(tweet)
     print(status.id)
 
-tweet_something("Hello World!")
+tweet_something("Hello World! I'm testing out a little twitter bot. Come check out the project at: https://github.com/Matthew-Bain-Projects/Twitter_Bot")
